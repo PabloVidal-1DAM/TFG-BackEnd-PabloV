02012abd8a8b2336e8c63c0c5849fe4a6d431960
PabloVidal-1DAM <pabloskyorito@gmail.com>
2026-02-25 18:38:02 +0100

    25/02/2026

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+   echo 'Proyecto Laravel para TFG de 2 de desarrollo de aplicaciones web.';
+});
